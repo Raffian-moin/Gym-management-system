b21071e03dd914446efff13da5dc50aa2742c96d
Raffian-moin <raffianmoin69@gmail.com>
2022-03-09 16:38:02 +0600

    mastering layout

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/logo.png" rel="icon">
+  <title> @yield('title') </title>
+  <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('assets/css/ruang-admin.min.css') }}" rel="stylesheet">
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+    @include('admin.layouts.sidebar')
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+            <!-- TopBar -->
+            @include('admin.layouts.nav')
+            <!-- Topbar -->
+
+            <!-- Container Fluid-->
+            <div class="container-fluid" id="container-wrapper">
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mb-0 text-gray-800"> @yield('page-title') </h1>
+                    @yield('breadcumb')
+                </div>
+
+                <div class="row mb-3">
+                    @yield('content')
+                </div>
+                <!--Row-->
+            </div>
+            <!---Container Fluid-->
+        </div>
+        <!-- Footer -->
+        @include('admin.layouts.footer')
+        <!-- Footer -->
+    </div>
+  </div>
+
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('assets/js/ruang-admin.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/chart.js/Chart.min.js') }}"></script>
+  <script src="{{ asset('assets/js/demo/chart-area-demo.js') }}"></script>  
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Admin\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Clear Application caches
+
+Route::get('/clear', function () {
+    Artisan::call('optimize:clear');
+    echo "View cache cleared! <br>";
+    echo "Route cache cleared! <br>";
+    echo "Configuration cache cleared! <br>";
+    echo "Application cache cleared! <br>";
+    echo "Cached events cleared! <br>";
+    echo "Compiled services and packages files removed! <br>";
+    echo "Caches cleared successfully! <br>";
+});
+
+Route::get('/admin/dashboard', [\App\Http\Controllers\Admin\HomeController::class, 'index']);
